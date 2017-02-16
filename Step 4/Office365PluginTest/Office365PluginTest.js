c17e3effe8119bfc56c9753d6d078629e92b8afb
dmarkovtsev <maverick-@users.noreply.github.com>
2017-02-16 18:00:26 +0300

    Outlook launch and termination

--- Step 4/Office365PluginTest/Office365PluginTest.js
+++ Step 4/Office365PluginTest/Office365PluginTest.js
@@ -3,6 +3,8 @@
 
 function Test()
 {
+	var pid = Global.DoLaunch('C:\\Program Files (x86)\\Microsoft Office\\root\\Office16\\Outlook.exe');
+
 	var userName = Common_GetParameter("UserName");
 	SeS('Mail_Folders')._DoClickNode(userName + ";Inbox");
 	var todayGroup = SeS('Group_By__Expanded__Date__Today');
@@ -16,9 +18,14 @@ function Test()
 			{
 				firstEmail.DoClick();
 				SeS('TextMiner').DoAction();
+				
+				//TODO
+				Global.DoSleep(10000);
 			}
 		}
 	}
+	
+	Global.DoKillByPid(pid);
 
 }
 
